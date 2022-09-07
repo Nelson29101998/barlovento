c41230c5164467ce0fe19a9ae2ceb40a90e3c443
Nelson29101998 <65300138+Nelson29101998@users.noreply.github.com>
2022-09-07 13:23:10 -0300

    Add files via upload

---
+++ pagina/menusAdm/arreglarAdm/ordenarTabla/informacionSalud.php
@@ -0,0 +1,39 @@
+<table>
+    <thead>
+        <tr>
+            <th>
+                <h5>Información de Salud</h5>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th class="form-group">
+                <label for="preSalud" class="text-white">
+                    <i class="fas fa-hand-holding-medical"></i> Previsión de salud:
+                </label>
+                <input type="text" class="form-control" name="preSalud" id="preSalud" maxlength="50" placeholder="Ingresa su previsión de salud">
+            </th>
+            <th class="form-group">
+                <label for="alergia" class="text-white">
+                    <i class="fas fa-person-dots-from-line"></i> Alergias:
+                </label>
+                <input type="text" class="form-control" name="alergia" id="alergia" maxlength="50" placeholder="Ingresa su alergias">
+            </th>
+        </tr>
+        <tr>
+            <th class="form-group">
+                <label for="hospital" class="text-white">
+                    <i class="fas fa-hospital"></i> Clínica / Hospital de preferencia:
+                </label>
+                <input type="text" class="form-control" name="hospital" id="hospital" maxlength="50" placeholder="Ingresa su preferencia">
+            </th>
+            <th class="form-group">
+                <label for="segurosSalud" class="text-white">
+                    <i class="fas fa-heart-pulse"></i> Seguros de salud complementarios:
+                </label>
+                <input type="text" class="form-control" name="segurosSalud" id="segurosSalud" maxlength="100"placeholder="Ingresa su complementarios">
+            </th>
+        </tr>
+    </tbody>
+</table>
---
+++ pagina/menusUser/menu.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if (!isset($_SESSION["usuario"]) && !isset($_SESSION["rut"])) {
+    header("location: ../../../../inicio.html");
+} else {
+?>
+    <?php
+    $use = $_SESSION["usuario"];
+    $_SESSION["usuario"] = $use;
+
+    $rut = $_SESSION["rut"];
+    $_SESSION["rut"] = $rut;
+    ?>
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../disenoMejor/bootstrap/css/bootstrap.min.css">
+        <script src="../../disenoMejor/bootstrap/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="../../disenoMejor/fontawesome/css/all.min.css">
+        <script script="../../disenoMejor/fontawesome/js/all.min.js"></script>
+
+        <link rel="stylesheet" href="../../disenoMejor/MDBootstrap/css/mdb.min.css">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        <title>Menu</title>
+        <link rel="icon" type="image/png" href="../../image/icon_Barlovento.png" />
+        <style>
+            body {
+                background-color: #2689F9;
+                color: white;
+            }
+        </style>
+    </head>
+
+    <body>
+        <?php
+        //*SideNav
+        include_once "navMenus/sideNav.php";
+        ?>
+        <br>
+        <div class="container text-center">
+            <h1>¡Bienvenido <span><?php echo $_SESSION['nombre']; ?>!</span></h1>
+            <br>
+
+        </div>
+    </body>
+
+    </html>
+<?php
+}
+?>
