ef429241df7c7275901286aac6e42cbc976cd655
Nelson29101998 <65300138+Nelson29101998@users.noreply.github.com>
2022-10-25 17:18:32 -0300

    Add files via upload

--- ajuste/favicon.php
+++ ajuste/favicon.php
@@ -1,7 +1,7 @@
-<link rel="apple-touch-icon" sizes="180x180" href="image/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="image/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="image/favicon/favicon-16x16.png">
-<link rel="manifest" href="image/favicon/site.webmanifest">
-<link rel="mask-icon" href="image/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="apple-touch-icon" sizes="180x180" <?php echo 'href="'.$direccion.'image/favicon/apple-touch-icon.png"';?>>
+<link rel="icon" type="image/png" sizes="32x32" <?php echo 'href="'.$direccion.'image/favicon/favicon-32x32.png"';?>>
+<link rel="icon" type="image/png" sizes="16x16" <?php echo 'href="'.$direccion.'image/favicon/favicon-16x16.png"'?>>
+<link rel="manifest" <?php echo 'href="'.$direccion.'image/favicon/site.webmanifest"'?>>
+<link rel="mask-icon" <?php echo 'href="'.$direccion.'image/favicon/safari-pinned-tab.svg" color="#5bbad5"'?>>
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
