2f9c73dbfb9960cdd10174c84e245783c6dda6e3
Nelson29101998 <65300138+Nelson29101998@users.noreply.github.com>
2023-03-25 17:17:35 -0300

    Add files via upload

--- conectarSQL/database.php
+++ conectarSQL/database.php
@@ -1,4 +1,13 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Max-Age: 1728000');
+header('Content-Length: 0');
+header('Content-Type: text/plain');
+
+
 $servername = "y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "oa7wrq5e1ellsws1";
 $password = "whwr5q1ob85q16mn";
